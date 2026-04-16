25c7a83e2cc4d3b346d70e16134985b53b906a1e
Robin Finkelmann <robin.finkelmann@gmail.com>
2026-04-16 14:57:07 +0200

    fix hyprland

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -347,10 +347,10 @@
         ", XF86AudioPrev, exec, playerctl previous"
       ];
 
-      windowrule = [
-        "suppressevent maximize, class:.*"
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
+      #windowrule = [
+      #  "suppressevent maximize, class:.*"
+      #  "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      #];
     };
   };
 }
